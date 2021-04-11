9ab51f11b1f8522c995eb80683599ed0bd330cab
Iryna <iryna.tar18@gmail.com>
2021-04-10 19:11:48 -0600

    added intro endpoint, deleted script.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('To start the game go to the url http://localhost/intro. Change user with your user name.')
+  res.send('To start the game go to the url http://localhost/intro.')
 })
 
 
 app.get('/intro', (req, res) => {
-  res.send('Today is a day!......')
+  res.send('Today is a  wonderful day! You and your sister are going to Paris to visit your relatives. You grab your bags and dump them in the boot of the Uber that is taking you to the airport. You clamber into the backseat with your sister and fall into a light sleep. You are quietly snoring when you hear the tyres skid to a stop. Something in your gut tells you that you are not safe anymore.')
 })
 
 
