62b136bef9fd92f22eb654cc6aa9ce707a729c2a
Iryna <iryna.tar18@gmail.com>
2021-04-10 18:45:33 -0600

    init server

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Choice 1: ')
+  res.send('To start the game go to the url http://localhost/intro. Change user with your user name.')
 })
 
 
 app.get('/intro', (req, res) => {
-    res.send('To start the game go to the url http://localhost/step1?player={user}. Change user with your user name.')
-  })
+  res.send('Today is a day!......')
+})
 
 
-  app.get('/step1', (req, res) => {
-    user = req.query['player']
-    res.send('To start game go to the url http://localhost/step2?player={user}. Change user with your user name.')
-  })
+app.get('/step1', (req, res) => {
+  user = req.query['player']
+  res.send('To start game go to the url http://localhost/step2?player={user}. Change user with your user name.')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
